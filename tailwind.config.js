f5b83d0304501634e36632487bfd6d6c1ec2bbc8
Vanshika Agrawal <vanshikaagrawal427@gmail.com>
2026-04-19 19:46:58 +0530

    add portfolio files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
+  darkMode: 'class',   // ✅ THIS IS REQUIRED
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
